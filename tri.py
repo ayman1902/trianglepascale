a84e9256778cb0fa31ae1670b4461c0dec39f9e7
ayman1902 <49163010+ayman1902@users.noreply.github.com>
2020-10-21 23:07:48 +0100

    Add files via upload

---
+++ tri.py
@@ -0,0 +1,15 @@
+def trianglePascal(n):
+    T = [[0] * (n+1) for p in range(n+1)]
+    for p in range(n+1):
+        if n == 0:
+            T[p][0] = 1
+        else:
+            for k in range(n+1):
+                if k == 0:
+                    T[p][0] = 1
+                else:
+                    T[p][k] = T[p-1][k-1] + T[p-1][k]
+    for i in range(len(T)):
+        print(T[i])
+
+trianglePascal(5)
